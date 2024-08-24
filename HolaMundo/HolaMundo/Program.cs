2a3d60dd4bcc2a8a8005bd2c35c566b297889bc9
David Espinoza <165340445+DavidEspinoza677@users.noreply.github.com>
2024-08-24 14:02:47 -0600

    Ejercicios practicos 19/08/24

---
+++ HolaMundo/HolaMundo/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace HolaMundo
+ {
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hola desde la UAM");
+            Console.WriteLine("Mi nombre es David Espinoza");
+            Console.WriteLine("Soy estudiante de primer año de Sistemas.");
+           
+    }
+}
