46075aaa35b3105be4a2ede4ee39ce7096933a39
David Espinoza <165340445+DavidEspinoza677@users.noreply.github.com>
2024-10-18 16:34:02 -0600

    Prueba ejer 1 181024

---
+++ Prueba Ejer1 181024/Prueba Ejer1/Prueba Ejer1/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace Prueba_Ejer1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtApellido = new System.Windows.Forms.TextBox();
+            this.txtNotaFinal = new System.Windows.Forms.TextBox();
+            this.Label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnOrdenar = new System.Windows.Forms.Button();
+            this.lstEstudiantes = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(83, 72);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(100, 20);
+            this.txtNombre.TabIndex = 0;
+            // 
+            // txtApellido
+            // 
+            this.txtApellido.Location = new System.Drawing.Point(83, 116);
+            this.txtApellido.Name = "txtApellido";
+            this.txtApellido.Size = new System.Drawing.Size(100, 20);
+            this.txtApellido.TabIndex = 1;
+            // 
+            // txtNotaFinal
+            // 
+            this.txtNotaFinal.Location = new System.Drawing.Point(83, 160);
+            this.txtNotaFinal.Name = "txtNotaFinal";
+            this.txtNotaFinal.Size = new System.Drawing.Size(100, 20);
+            this.txtNotaFinal.TabIndex = 2;
+            // 
+            // Label1
+            // 
+            this.Label1.AutoSize = true;
+            this.Label1.Location = new System.Drawing.Point(19, 79);
+            this.Label1.Name = "Label1";
+            this.Label1.Size = new System.Drawing.Size(44, 13);
+            this.Label1.TabIndex = 3;
+            this.Label1.Text = "Nombre";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 119);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Apellido";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(19, 167);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Nota Final";
+            // 
+            // btnAgregar
+            // 
+            this.btnAgregar.Location = new System.Drawing.Point(83, 233);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 6;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.btnAgregar_Click_1);
+            // 
+            // btnOrdenar
+            // 
+            this.btnOrdenar.Location = new System.Drawing.Point(205, 233);
+            this.btnOrdenar.Name = "btnOrdenar";
+            this.btnOrdenar.Size = new System.Drawing.Size(75, 23);
+            this.btnOrdenar.TabIndex = 7;
+            this.btnOrdenar.Text = "Ordenar";
+            this.btnOrdenar.UseVisualStyleBackColor = true;
+            this.btnOrdenar.Click += new System.EventHandler(this.btnOrdenar_Click_1);
+            // 
+            // lstEstudiantes
+            // 
+            this.lstEstudiantes.FormattingEnabled = true;
+            this.lstEstudiantes.Location = new System.Drawing.Point(345, 49);
+            this.lstEstudiantes.Name = "lstEstudiantes";
+            this.lstEstudiantes.Size = new System.Drawing.Size(388, 173);
+            this.lstEstudiantes.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lstEstudiantes);
+            this.Controls.Add(this.btnOrdenar);
+            this.Controls.Add(this.btnAgregar);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Label1);
+            this.Controls.Add(this.txtNotaFinal);
+            this.Controls.Add(this.txtApellido);
+            this.Controls.Add(this.txtNombre);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtApellido;
+        private System.Windows.Forms.TextBox txtNotaFinal;
+        private System.Windows.Forms.Label Label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnOrdenar;
+        private System.Windows.Forms.ListBox lstEstudiantes;
+    }
+}
+
---
+++ Prueba Ejer1 181024/Prueba Ejer1/Prueba Ejer1/Form1.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prueba_Ejer1
+{
+    public partial class Form1 : Form
+    {
+
+        private List<Estudiante> estudiantes = new List<Estudiante>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnAgregar_Click(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            double notaFinal;
+
+
+            if (double.TryParse(txtNotaFinal.Text, out notaFinal))
+            {
+
+                Estudiante estudiante = new Estudiante(nombre, apellido, notaFinal);
+                estudiantes.Add(estudiante);
+
+
+                ActualizarListaEstudiantes();
+
+
+                LimpiarCampos();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese una nota válida.");
+            }
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+
+            Estudiante[] estudiantesArray = estudiantes.ToArray();
+            Ordenador.MergeSort(estudiantesArray, 0, estudiantesArray.Length - 1);
+            estudiantes = estudiantesArray.ToList();
+
+
+            ActualizarListaEstudiantes();
+        }
+
+
+        private void ActualizarListaEstudiantes()
+        {
+            lstEstudiantes.Items.Clear();
+            foreach (Estudiante estudiante in estudiantes)
+            {
+                lstEstudiantes.Items.Add(estudiante.ToString());
+            }
+        }
+
+
+        private void LimpiarCampos()
+        {
+            txtNombre.Text = "";
+            txtApellido.Text = "";
+            txtNotaFinal.Text = "";
+        }
+
+        private void btnAgregar_Click_1(object sender, EventArgs e)
+        {
+            string nombre = txtNombre.Text;
+            string apellido = txtApellido.Text;
+            double notaFinal;
+
+            
+            if (double.TryParse(txtNotaFinal.Text, out notaFinal))
+            {
+                
+                Estudiante estudiante = new Estudiante(nombre, apellido, notaFinal);
+                estudiantes.Add(estudiante);
+
+                
+                ActualizarListaEstudiantes();
+
+                
+                LimpiarCampos();
+            }
+            else
+            {
+                MessageBox.Show("Ingrese una nota válida.");
+            }
+        }
+
+        private void btnOrdenar_Click_1(object sender, EventArgs e)
+        {
+            
+            Estudiante[] estudiantesArray = estudiantes.ToArray();
+            Ordenador.MergeSort(estudiantesArray, 0, estudiantesArray.Length - 1);
+            estudiantes = estudiantesArray.ToList();
+
+            
+            ActualizarListaEstudiantes();
+        }
+    }
+
+
+    public struct Estudiante
+    {
+        public string Nombre;
+        public string Apellido;
+        public double NotaFinal;
+
+        public Estudiante(string nombre, string apellido, double notaFinal)
+        {
+            Nombre = nombre;
+            Apellido = apellido;
+            NotaFinal = notaFinal;
+        }
+
+
+        public string ObtenerCalificacionLetra()
+        {
+            if (NotaFinal >= 90)
+                return "A";
+            else if (NotaFinal >= 80)
+                return "B";
+            else if (NotaFinal >= 70)
+                return "C";
+            else if (NotaFinal >= 60)
+                return "D";
+            else
+                return "F";
+        }
+
+
+        public override string ToString()
+        {
+            return $"{Nombre} {Apellido}, Nota Final: {NotaFinal}, Calificación: {ObtenerCalificacionLetra()}";
+        }
+    }
+
+
+    public class Ordenador
+    {
+
+        public static void MergeSort(Estudiante[] estudiantes, int left, int right)
+        {
+            if (left < right)
+            {
+                int middle = (left + right) / 2;
+
+                MergeSort(estudiantes, left, middle);
+                MergeSort(estudiantes, middle + 1, right);
+
+                Merge(estudiantes, left, middle, right);
+            }
+        }
+
+
+        private static void Merge(Estudiante[] estudiantes, int left, int middle, int right)
+        {
+            int n1 = middle - left + 1;
+            int n2 = right - middle;
+
+            Estudiante[] leftArray = new Estudiante[n1];
+            Estudiante[] rightArray = new Estudiante[n2];
+
+            Array.Copy(estudiantes, left, leftArray, 0, n1);
+            Array.Copy(estudiantes, middle + 1, rightArray, 0, n2);
+
+            int i = 0, j = 0, k = left;
+            while (i < n1 && j < n2)
+            {
+                if (leftArray[i].NotaFinal <= rightArray[j].NotaFinal)
+                {
+                    estudiantes[k] = leftArray[i];
+                    i++;
+                }
+                else
+                {
+                    estudiantes[k] = rightArray[j];
+                    j++;
+                }
+                k++;
+            }
+
+            while (i < n1)
+            {
+                estudiantes[k] = leftArray[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                estudiantes[k] = rightArray[j];
+                j++;
+                k++;
+            }
+        }
+    }
+}
